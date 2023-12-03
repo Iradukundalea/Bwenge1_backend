17a3b2dbc829f920fc9651b6eefbdfd29a113603
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-03 16:17:14 +0200

    reset base url

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,16 @@ import { uploadImageFile } from "../middleware/ProfilePicturesS3.js";
 import sharp from "sharp";
 
 export const register = async (req, res, next) => {
-  const { firstName, lastName, birthDate, phoneNumber, email, password, institution, gender } = req.body;
+  const {
+    firstName,
+    lastName,
+    birthDate,
+    phoneNumber,
+    email,
+    password,
+    institution,
+    gender,
+  } = req.body;
   const role = "user";
   const user = await User.findOne({ email });
   if (user) {
@@ -36,7 +45,9 @@ export const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ErrorResponse("Please provide an email and a password", 400));
+    return next(
+      new ErrorResponse("Please provide an email and a password", 400)
+    );
   }
   try {
     const user = await User.findOne({ email }).select("+password");
@@ -59,13 +70,15 @@ export const forgotpassword = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return next(new ErrorResponse("Email could not be found,Create Account", 404));
+      return next(
+        new ErrorResponse("Email could not be found,Create Account", 404)
+      );
     }
 
     const resetToken = user.getResetPasswordToken();
     await user.save();
 
-    const resetUrl = `http://bwengeserver.com/passwordreset/${resetToken}`;
+    const resetUrl = `${process.env.RESET_BASE_URL}/passwordreset/${resetToken}`;
 
     const message = `
         <h1>You have requested a password reset</h1>
@@ -97,13 +110,15 @@ export const Requestverifyemail = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return next(new ErrorResponse("Email could not be found,Create Account", 404));
+      return next(
+        new ErrorResponse("Email could not be found,Create Account", 404)
+      );
     }
 
     const resetToken = user.getConfirmEmailToken();
     await user.save();
 
-    const resetUrl = `http://bwengeserver.com/confirmemail/${resetToken}`;
+    const resetUrl = `${process.env.RESET_BASE_URL}/confirmemail/${resetToken}`;
 
     const message = `
         <h1>Bwenge User Email Confirmation</h1>
@@ -130,7 +145,10 @@ export const Requestverifyemail = async (req, res, next) => {
   }
 };
 export const resetpassword = async (req, res, next) => {
-  const resetPasswordToken = crypto.createHash("sha256").update(req.params.resetToken).digest("hex");
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
 
   try {
     const user = await User.findOne({
@@ -155,7 +173,10 @@ export const resetpassword = async (req, res, next) => {
 };
 export const verifyemail = async (req, res, next) => {
   console.log("ok ok ok");
-  const confirmEmailToken = crypto.createHash("sha256").update(req.params.verifytoken).digest("hex");
+  const confirmEmailToken = crypto
+    .createHash("sha256")
+    .update(req.params.verifytoken)
+    .digest("hex");
   console.log(confirmEmailToken);
 
   try {
@@ -195,7 +216,17 @@ export const verifyUser = async (req, res, next) => {
 };
 
 export const addInstitutionUser = async (req, res, next) => {
-  const { prefix, firstName, lastName, birthDate, gender, phoneNumber, email, password, institution } = req.body;
+  const {
+    prefix,
+    firstName,
+    lastName,
+    birthDate,
+    gender,
+    phoneNumber,
+    email,
+    password,
+    institution,
+  } = req.body;
   console.log({
     firstName,
     lastName,
@@ -266,7 +297,9 @@ export const updateProfilePicture = async (req, res, next) => {
       uploadImageFile(buff, uploadPath);
     });
   try {
-    const updatedUserProfile = await User.findByIdAndUpdate(id, { profilePicture: uploadPath });
+    const updatedUserProfile = await User.findByIdAndUpdate(id, {
+      profilePicture: uploadPath,
+    });
     res.status(200).json({ success: true, message: "profile updated" });
   } catch (error) {
     next(error);
